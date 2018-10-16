b20e4fc28f2dcde76ed3f7befd850c0507e177e9
Руслан Муфтиев <muftiev.rr@ya.ru>
2018-10-16 22:26:21 +0300

    add tests for CommentList

--- src/components/comment-list.js
+++ src/components/comment-list.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Comment from './comment'
 import toggleOpen from '../decorators/toggleOpen'
 
-class CommentList extends Component {
+export class CommentList extends Component {
   /*
   static defaultProps = {
     comments: []
@@ -20,27 +20,28 @@ class CommentList extends Component {
     const text = isOpen ? 'hide comments' : 'show comments'
     return (
       <div>
-        <button onClick={toggleOpen}>{text}</button>
+        <button onClick={toggleOpen} className="test--comment-list__btn">
+          {text}
+        </button>
         {this.getBody()}
       </div>
     )
   }
 
   getBody() {
-    //    const { comments = [], isOpen } = this.props
-    const { comments, isOpen } = this.props
+    const { comments = [], isOpen } = this.props
     if (!isOpen) return null
 
     const body = comments.length ? (
-      <ul>
+      <ul className="test--comment-list__body">
         {comments.map((comment) => (
-          <li key={comment.id}>
+          <li key={comment.id} className="test--comment-list__item">
             <Comment comment={comment} />
           </li>
         ))}
       </ul>
     ) : (
-      <h3>No comments yet</h3>
+      <h3 className="test--comment-list__empty">No comments yet</h3>
     )
 
     return <div>{body}</div>
---
+++ src/components/comment-list.test.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Enzyme, { render, shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import CommentListWithToggleOpen, { CommentList } from './comment-list'
+import articles from '../fixtures'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('CommentList', () => {
+  const comments = articles[0].comments
+
+  it('should render items', () => {
+    const container = shallow(<CommentList comments={comments} isOpen={true} />)
+
+    expect(container.find('.test--comment-list__item').length).toEqual(
+      comments.length
+    )
+  })
+
+  it('should render "No comments yet" string for falsy comments prop', () => {
+    const container = shallow(<CommentList comments={[]} isOpen={true} />)
+
+    expect(container.find('.test--comment-list__body').length).toEqual(0)
+    expect(container.find('.test--comment-list__empty').length).toEqual(1)
+  })
+
+  it('should render closed comments by default', () => {
+    const container = render(<CommentListWithToggleOpen comments={comments} />)
+
+    expect(container.find('.test--comment-list__body').length).toEqual(0)
+  })
+
+  it('should open comments on click', () => {
+    const container = mount(<CommentListWithToggleOpen comments={comments} />)
+
+    expect(container.find('.test--comment-list__body').length).toEqual(0)
+
+    container
+      .find('.test--comment-list__btn')
+      .at(0)
+      .simulate('click')
+
+    expect(container.find('.test--comment-list__body').length).toEqual(1)
+  })
+})
