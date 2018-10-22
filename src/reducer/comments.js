c1f74b1070fa20b4a90161c2dd39d3fd05150960
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-10-22 20:41:57 +0300

    add complex selector

---
+++ src/reducer/comments.js
@@ -0,0 +1,11 @@
+import {} from '../constants'
+import { normalizedComments as defaultComments } from '../fixtures'
+
+export default (state = defaultComments, action) => {
+  const { type } = action
+
+  switch (type) {
+    default:
+      return state
+  }
+}
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -3,6 +3,8 @@ import { createSelector } from 'reselect'
 export const selectionSelector = (state) => state.filters.selected
 export const dateRangeSelector = (state) => state.filters.dateRange
 export const articleListSelector = (state) => state.articles
+export const commentsSelector = (state) => state.comments
+export const idSelector = (_, props) => props.id
 
 export const filtratedArticlesSelector = createSelector(
   selectionSelector,
@@ -22,3 +24,9 @@ export const filtratedArticlesSelector = createSelector(
     })
   }
 )
+
+export const createCommentSelector = () =>
+  createSelector(commentsSelector, idSelector, (comments, id) => {
+    console.log('---', 'comment selector', id)
+    return comments.find((comment) => comment.id === id)
+  })
